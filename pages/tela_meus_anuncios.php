1c202b3e4fe5d4ddda631eccbde650219fbfd9d8
Joabe Nascimento <63386616+devs-joabe@users.noreply.github.com>
2023-04-28 01:18:22 -0300

    novas-telas

---
+++ pages/tela_meus_anuncios.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Meus Anúncios</title>
+    <link rel="stylesheet" href="../css//tela_meus_anuncios.css">
+  </head>
+  <body>
+    <header>
+      <h1>Meus Anúncios</h1>
+    </header>
+    
+    <section class="anuncios">
+      <div class="anuncio">
+        <img src="../img/casa.png" alt="Anúncio 1">
+        <h2>Anúncio 1</h2>
+        <p>Descrição do anúncio 1.</p>
+        <a href="#">Saiba mais</a>
+      </div>
+      
+      <div class="anuncio">
+        <img src="../img/casa.png" alt="Anúncio 2">
+        <h2>Anúncio 2</h2>
+        <p>Descrição do anúncio 2.</p>
+        <a href="#">Saiba mais</a>
+      </div>
+      
+      <div class="anuncio">
+        <img src="../img/casa.png" alt="Anúncio 3">
+        <h2>Anúncio 3</h2>
+        <p>Descrição do anúncio 3.</p>
+        <a href="#">Saiba mais</a>
+      </div>
+      
+      <div class="anuncio">
+        <img src="../img/casa.png" alt="Anúncio 4">
+        <h2>Anúncio 4</h2>
+        <p>Descrição do anúncio 4.</p>
+        <a href="#">Saiba mais</a>
+        <a style="background-color: green; " href="#">Editar</a>
+        <a style="background-color: red;" href="#">Excluir</a>
+      </div>
+      
+      <div class="anuncio">
+        <img src="../img/casa.png" alt="Anúncio 5">
+        <h2>Anúncio 5</h2>
+        <p>Descrição do anúncio 5.</p>
+        <a href="#">Saiba mais</a>
+      </div>
+      
+      <div class="anuncio">
+        <img src="../img/casa.png" alt="Anúncio 6">
+        <h2>Anúncio 6</h2>
+        <p>Descrição do anúncio 6.</p>
+        <a href="#">Saiba mais</a>
+      </div>
+    </section>
+    
+    <footer>
+      <p>&copy; 2023 Meus Anúncios. Todos os direitos reservados.</p>
+    </footer>
+  </body>
+</html>
