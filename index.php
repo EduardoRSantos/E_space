0042f62f474dd6e7f9dca4156331540d82873278
Joabe Nascimento <63386616+devs-joabe@users.noreply.github.com>
2023-05-03 22:37:02 -0300

    atualizado

--- index.php
+++ index.php
@@ -99,6 +99,9 @@
 
   </main>
   <div class="wrapper" >  
+    <div class="column" >
+
+
   <?php
   if (empty($_POST['pesquisar'])) {
 
@@ -156,6 +159,7 @@
   }
 
   ?>
+      </div>
   </div>
 
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
