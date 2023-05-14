c9853d64400c23b4cb933ef4d55b1741d587365a
EduardoRSantos <eduardoow6@gmail.com>
2023-05-14 10:03:55 -0300

    eduardo

--- env.php
+++ env.php
@@ -2,8 +2,8 @@
 
 putenv('DISPLAY_ERROS_DETAILS='. true);
 
-putenv('E_SPACE_HOST=mysql.espace.kinghost.net');
-putenv('E_SPACE_PORT=3306');
-putenv('E_SPACE_USER=espace');
-putenv('E_SPACE_PASSWORD=espace2710');
-putenv('E_SPACE_DBNAME=espace');
+// putenv('E_SPACE_HOST=mysql.espace.kinghost.net');
+// putenv('E_SPACE_PORT=3306');
+// putenv('E_SPACE_USER=espace');
+// putenv('E_SPACE_PASSWORD=espace2710');
+// putenv('E_SPACE_DBNAME=espace');
