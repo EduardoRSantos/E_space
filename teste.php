fdbcd25a8c2fe232bd8aab97c9fc2dc59d794b93
EduardoRSantos <eduardoow6@gmail.com>
2023-05-18 22:13:08 -0300

    teste

--- teste.php
+++ teste.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once './routes/index.php';
+
 // $host = "mysql.espace.kinghost.net";
 // $port = "3306";
 // $user = "espace";
@@ -29,18 +31,18 @@
 //         $dbname = "e_space";
 
 
-$curl = curl_init();
+// $curl = curl_init();
 
-curl_setopt_array($curl, [
-    CURLOPT_URL => 'http://www.espace.kinghost.net/routes/index.php/usuarios',
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_RETURNTRANSFER => true,
-]);
+// curl_setopt_array($curl, [
+//     CURLOPT_URL => 'http://localhost/E_space/routes/index.php/usuarios',
+//     CURLOPT_CUSTOMREQUEST => "GET",
+//     CURLOPT_RETURNTRANSFER => true,
+// ]);
 
-$response = curl_exec($curl);
+// $response = curl_exec($curl);
 
-$data = json_decode($response, true);
+// $data = json_decode($response, true);
 
-curl_close($curl);
+// curl_close($curl);
 
-var_dump($data);
+// var_dump($data);
