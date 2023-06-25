059c2ed68bbca7e97f47dc9da18dd89cab14bf74
EduardoRSantos <eduardoow6@gmail.com>
2023-06-25 18:01:54 -0300

    hospedar

--- routes/index.php
+++ routes/index.php
@@ -1,10 +1,6 @@
 <?php
 
 
-require_once  './vendor/autoload.php';
-// require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../src/SlimConfiguration.php';
-require_once __DIR__ . '/../env.php';
 
 use function src\{SlimConfiguration};
 use App\Controllers\{UsuarioController, AnuncioController};
