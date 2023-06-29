333fbb20e8630f211d8b63ecbb962708f3c7c125
devs-joabe <joabefnascimento1@outlook.com>
2023-06-28 23:28:16 -0300

    tela-atualizada-1

--- avaliar_anuncios/anuncios_avaliar.php
+++ avaliar_anuncios/anuncios_avaliar.php
@@ -77,6 +77,8 @@
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal<?= $anuncio['id'] ?>">imagens</button>
             <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modalAceita<?= $anuncio['id'] ?>">Aceitar</button>
             <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalNegar<?= $anuncio['id'] ?>">Negar</button>
+            <button type="button" class="btn btn-danger" id="btnAnuncios"></button>
+
           </div>
               </div>
             </div>
@@ -177,10 +179,20 @@
   } ?>
 
 <?php include '../footer.php'; ?>
+
+<script>
+  document.getElementById('btnAnuncios').addEventListener('click', function() {
+    window.location.href = '../avaliar_anuncios/anuncios_postados.php';
+  });
+</script>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+<script>
+  document.getElementById('btnAnuncios').addEventListener('click', function() {
+    window.location.href = '../avaliar_anuncios/anuncios_postados.php';
+  });
+</script>
 </body>
 
 <?php
